6cfbd9d633bd17d42e87b56603b071707c34ffd0
Seunghwa Kang <seunghwak@nvidia.com>
2020-01-16 13:34:00 -0800

    removed internal function import statements from __init__.py

--- python/cusignal/__init__.py
+++ python/cusignal/__init__.py
@@ -1,14 +1,6 @@
 from cusignal.signaltools import (
-    _inputs_swap_needed,
     correlate,
-    _centered,
     fftconvolve,
-    _numeric_arrays,
-    _prod,
-    _fftconv_faster,
-    _reverse_and_conj,
-    _np_conv_ok,
-    _timeit_fast,
     choose_conv_method,
     convolve,
     wiener,
@@ -25,9 +17,6 @@ from cusignal.signaltools import (
     freq_shift
 )
 from cusignal.windows import (
-    _len_guards,
-    _extend,
-    _truncate,
     general_cosine,
     boxcar,
     triang,
@@ -49,15 +38,10 @@ from cusignal.windows import (
     chebwin,
     cosine,
     exponential,
-    _fftautocorr,
     get_window
 )
-from cusignal.fir_filter_design import (
-    _get_fs, kaiser_beta, kaiser_atten, firwin
-)
-from cusignal.fftpack_helper import (
-    next_fast_len, _init_nd_shape_and_axes, _init_nd_shape_and_axes_sorted
-)
+from cusignal.fir_filter_design import kaiser_beta, kaiser_atten, firwin
+from cusignal.fftpack_helper import next_fast_len 
 from cusignal.spectral import (
     lombscargle,
     periodogram,
@@ -65,31 +49,17 @@ from cusignal.spectral import (
     csd,
     spectrogram,
     stft,
-    coherence,
-    _spectral_helper,
-    _fft_helper,
-    _triage_segments,
-    _median_bias
+    coherence
 )
 from cusignal.bsplines import (
     gauss_spline,
     cubic,
     quadratic,
-    _coeff_smooth,
-    _hc,
-    _hs,
-    _cubic_smooth_coeff,
-    _cubic_coeff,
-    _quadratic_coeff,
     cspline1d
 )
-from cusignal.waveforms import (
-    square, gausspulse, chirp, _chirp_phase, unit_impulse
-)
+from cusignal.waveforms import square, gausspulse, chirp, unit_impulse
 from cusignal.wavelets import qmf, morlet, ricker, cwt
-from cusignal._peak_finding import (
-    _boolrelextrema, argrelmin, argrelmax, argrelextrema
-)
+from cusignal._peak_finding import argrelmin, argrelmax, argrelextrema
 from cusignal._upfirdn import upfirdn
 from cusignal._arraytools import (
     get_shared_array,
@@ -101,20 +71,6 @@ from cusignal._arraytools import (
     const_ext,
     zero_ext,
     as_strided
-)
-from cusignal._signaltools import (
-    _valfrommode,
-    _bvalfromboundary,
-    _inputs_swap_needed,
-    _iDivUp,
-    _correlate2d_odd,
-    _correlate2d_even,
-    _correlate2d_ns,
-    _convolve2d_odd,
-    _convolve2d_even,
-    _convolve2d_ns,
-    _convolve2d_gpu,
-    _convolve2d
 )
 from cusignal.cupy_helper import polyval, toeplitz, hankel
 
