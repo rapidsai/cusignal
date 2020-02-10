bb134fee88e67400c7ae1e27a362f4ea0079bc96
Raymond Douglass <ray@raydouglass.com>
2020-02-10 11:04:37 -0500

    ENH Add ci and conda recipe

---
+++ ci/gpu/build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Copyright (c) 2018-2019, NVIDIA CORPORATION.
+#############################################
+# cuSignal GPU build and test script for CI #
+#############################################
+set -e
+NUMARGS=$#
+ARGS=$*
+
+# Logger function for build status output
+function logger() {
+  echo -e "\n>>>> $@\n"
+}
+
+# Arg parsing function
+function hasArg {
+    (( ${NUMARGS} != 0 )) && (echo " ${ARGS} " | grep -q " $1 ")
+}
+
+# Set path and build parallel level
+export PATH=/conda/bin:/usr/local/cuda/bin:$PATH
+export PARALLEL_LEVEL=4
+export CUDA_REL=${CUDA_VERSION%.*}
+
+# Set home to the job's workspace
+export HOME=$WORKSPACE
+
+# Parse git describei
+cd $WORKSPACE
+export GIT_DESCRIBE_TAG=`git describe --tags`
+export MINOR_VERSION=`echo $GIT_DESCRIBE_TAG | grep -o -E '([0-9]+\.[0-9]+)'`
+
+################################################################################
+# SETUP - Check environment
+################################################################################
+
+logger "Check environment..."
+env
+
+logger "Check GPU usage..."
+nvidia-smi
+
+logger "Activate conda env..."
+source activate gdf
+conda install -c rapidsai -c rapidsai-nightly -c nvidia -c conda-forge \
+    cudatoolkit=${CUDA_REL} \
+    "scipy>=1.3.0" \
+    "numpy>=1.17.3" \
+    boost \
+    "numba>=0.46.0" \
+    "cupy>=6.2.0"
+
+logger "Check versions..."
+python --version
+$CC --version
+$CXX --version
+conda list
+
+################################################################################
+# TEST - Run GoogleTest and py.tests for cusignal
+################################################################################
+
+if hasArg --skip-tests; then
+    logger "Skipping Tests..."
+    exit 0
+fi
+
+logger "Check GPU usage..."
+nvidia-smi
+
+logger "Python pytest for cusignal..."
+cd $WORKSPACE/python
+
+pytest --cache-clear --junitxml=${WORKSPACE}/junit-cusignal.xml -v -s
