77251ed2b0f43e33d578451d03865e58f2a6f2cb
Adam Thompson <adamt@nvidia.com>
2020-03-05 08:27:49 -0500

    Adding real cepstrum

--- cusignal/acoustics.py
+++ cusignal/acoustics.py
@@ -1,9 +1,9 @@
 import cupy as cp
-import cupyx.scipy.fftpack.fft as fft
+import cupyx.scipy.fftpack as fft
 
-def cepstrum(x, nfft=None, mode=None, axis=-1):
+def rceps(x, nfft=None, axis=-1):
     r"""
-    Calculates the cepstrum of an input sequence x where the cepstrum is
+    Calculates the real cepstrum of an input sequence x where the cepstrum is
     defined as the inverse Fourier transform of the log magnitude DFT (spectrum)
     of a signal. It's primarily used for source/speaker separation in speech
     signal processing
@@ -13,8 +13,6 @@ def cepstrum(x, nfft=None, mode=None, axis=-1):
         Input sequence, if x is a matrix, return cepstrum in direction of axis
     nfft : int
         Size of Fourier Transform
-    mode: {real, complex}
-        Real/Complex cepstrum result desired
     axis: int
         Direction for cepstrum calculation
     Returns
