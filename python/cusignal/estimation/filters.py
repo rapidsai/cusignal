753a6aec06d6e08526cddee58a2a8dde169d7296
Matthew Nicely <mnicely@nvidia.com>
2020-07-15 10:46:17 -0400

    Updated KF docs

--- python/cusignal/estimation/filters.py
+++ python/cusignal/estimation/filters.py
@@ -20,20 +20,178 @@ from . import _filters
 class KalmanFilter(object):
 
     """
-    TBD
+    This is a multi-point Kalman Filter implementation of
+    https://github.com/rlabbe/filterpy/blob/master/filterpy/kalman/kalman_filter.py,
+    with a subset of functionality.
+
+    All Kalman Filter matrices are stack on the X axis. This is to allow
+    for optimal global accesses on the GPU.
 
     Parameters
     ----------
-    in : TBD
+    dim_x : int
+        Number of state variables for the Kalman filter. For example, if
+        you are tracking the position and velocity of an object in two
+        dimensions, dim_x would be 4.
+        This is used to set the default size of P, Q, and u
+    dim_z : int
+        Number of of measurement inputs. For example, if the sensor
+        provides you with position in (x,y), dim_z would be 2.
+    dim_u : int (optional)
+        Size of the control input, if it is being used.
+        Default value of 0 indicates it is not used.
+    points : int (optional)
+        Number of Kalman Filter points to track.
+    dtype : dtype (optional)
+        Data type of compute.
 
-    Returns
-    -------
-    out : TBD
+    Attributes
+    ----------
+    x : numpy.array(points, dim_x, 1)
+        Current state estimate. Any call to update() or predict() updates
+        this variable.
+
+    P : numpy.array(points, dim_x, dim_x)
+        Current state covariance matrix. Any call to update() or predict()
+        updates this variable.
+
+    z : numpy.array(points, dim_z, 1)
+        Last measurement used in update(). Read only.
+
+    R : numpy.array(points, dim_z, dim_z)
+        Measurement noise matrix
+
+    Q : numpy.array(points, dim_x, dim_x)
+        Process noise matrix
+
+    F : numpy.array(points, dim_x, dim_x)
+        State Transition matrix
+
+    H : numpy.array(points, dim_z, dim_x)
+        Measurement function
+
+    _alpha_sq : float (points, 1, 1)
+        Fading memory setting. 1.0 gives the normal Kalman filter, and
+        values slightly larger than 1.0 (such as 1.02) give a fading
+        memory effect - previous measurements have less influence on the
+        filter's estimates. This formulation of the Fading memory filter
+        (there are many) is due to Dan Simon [1]_.
+
+    Examples
+    --------
+    Here is a filter that tracks position and velocity using a sensor that only
+    reads position.
+
+    First construct the object with the required dimensionality,
+    number of points, and data type.
+
+    .. code::
+
+        import cupy as cp
+        import numpy as np
+
+        from cusignal import KalmanFilter
+
+        points = 1024
+        kf = KalmanFilter(dim_x=4, dim_z=2, points=points, dtype=cp.float64)
+
+    Assign the initial value for the state (position and velocity)
+    for all Kalman Filter points.
+
+    .. code::
+
+        initial_location = np.array(
+            [[10.0, 10.0, 0.0, 0.0]], dtype=dt
+        ).T  # x, y, v_x, v_y
+        kf.x = cp.repeat(
+            cp.asarray(initial_location[cp.newaxis, :, :]), points, axis=0
+        )
+
+    Define the state transition matrix for all Kalman Filter points:
+
+        .. code::
+
+            F = np.array(
+                [
+                    [1.0, 0.0, 1.0, 0.0],  # x = x0 + v_x*dt
+                    [0.0, 1.0, 0.0, 1.0],  # y = y0 + v_y*dt
+                    [0.0, 0.0, 1.0, 0.0],  # dx = v_x
+                    [1.0, 0.0, 0.0, 1.0],
+                ],  # dy = v_y
+                dtype=dt,
+            )
+            kf.F = cp.repeat(cp.asarray(F[cp.newaxis, :, :]), points, axis=0)
+
+    Define the measurement function for all Kalman Filter points:
+
+        .. code::
+
+            H = np.array(
+                [[1.0, 0.0, 1.0, 0.0], [0.0, 1.0, 0.0, 1.0]],
+                dtype=dt,  # x_0  # y_0
+            )
+            kf.H = cp.repeat(cp.asarray(H[cp.newaxis, :, :]), points, axis=0)
+
+    Define the covariance matrix for all Kalman Filter points:
+
+        .. code::
+
+            initial_estimate_error = np.eye(dim_x, dtype=dt) * np.array(
+                [1.0, 1.0, 2.0, 2.0], dtype=dt
+            )
+            kf.P = cp.repeat(
+                cp.asarray(initial_estimate_error[cp.newaxis, :, :]),
+                points,
+                axis=0,
+            )
+
+    Define the measurement noise  for all Kalman Filter points:
+
+        .. code::
+
+            measurement_noise = np.eye(dim_z, dtype=dt) * 0.01
+            kf.R = cp.repeat(
+                cp.asarray(measurement_noise[cp.newaxis, :, :]), points, axis=0
+            )
+
+    Define the process noise  for all Kalman Filter points:
+
+        .. code::
+            motion_noise = np.eye(dim_x, dtype=dt) * np.array(
+                [10.0, 10.0, 10.0, 10.0], dtype=dt
+            )
+            kf.Q = cp.repeat(
+                cp.asarray(motion_noise[cp.newaxis, :, :]), points, axis=0
+            )
+
+    Now just perform the standard predict/update loop:
+    Note: This example just uses the same sensor reading for all points
+
+        .. code::
+
+            kf.predict()
+            z = get_sensor_reading()
+            kf.z = cp.repeat(z[cp.newaxis, :, :], points, axis=0)
+            kf.update()
+
+    Results are in:
+
+        .. code::
+            kf.x[:, :, :]
+
+    References
+    ----------
+
+    .. [1] Dan Simon. "Optimal State Estimation." John Wiley & Sons.
+       p. 208-212. (2006)
+
+    .. [2] Roger Labbe. "Kalman and Bayesian Filters in Python"
+       https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python
 
     """
 
     def __init__(
-        self, num_points, dim_x, dim_z, dim_u=0, dtype=cp.float32,
+        self, dim_x, dim_z, dim_u=0, points=1, dtype=cp.float32,
     ):
 
         # Check CuPy version
@@ -43,7 +201,7 @@ class KalmanFilter(object):
                 "Kalman Filter only compatible with CuPy v.8.0.0b4+"
             )
 
-        self.num_points = num_points
+        self.points = points
 
         if dim_x < 1:
             raise ValueError("dim_x must be 1 or greater")
@@ -52,27 +210,22 @@ class KalmanFilter(object):
         if dim_u < 0:
             raise ValueError("dim_u must be 0 or greater")
 
-        if dim_z > 4:
-            raise ValueError(
-                "cuSignal KalmanFilter only works with dim_z = 2 currently"
-            )
-
         self.dim_x = dim_x
         self.dim_z = dim_z
         self.dim_u = dim_u
 
         # Create data arrays
-        self.x = cp.zeros((self.num_points, dim_x, 1,), dtype=dtype)  # state
+        self.x = cp.zeros((self.points, dim_x, 1,), dtype=dtype)  # state
 
         self.P = cp.repeat(
             cp.identity(dim_x, dtype=dtype)[cp.newaxis, :, :],
-            self.num_points,
+            self.points,
             axis=0,
         )  # uncertainty covariance
 
         self.Q = cp.repeat(
             cp.identity(dim_x, dtype=dtype)[cp.newaxis, :, :],
-            self.num_points,
+            self.points,
             axis=0,
         )  # process uncertainty
 
@@ -80,29 +233,25 @@ class KalmanFilter(object):
 
         self.F = cp.repeat(
             cp.identity(dim_x, dtype=dtype)[cp.newaxis, :, :],
-            self.num_points,
+            self.points,
             axis=0,
         )  # state transition matrix
 
         self.H = cp.zeros(
-            (self.num_points, dim_z, dim_z,), dtype=dtype
+            (self.points, dim_z, dim_z,), dtype=dtype
         )  # Measurement function
 
         self.R = cp.repeat(
             cp.identity(dim_z, dtype=dtype)[cp.newaxis, :, :],
-            self.num_points,
+            self.points,
             axis=0,
         )  # process uncertainty
 
         self._alpha_sq = cp.ones(
-            (self.num_points, 1, 1,), dtype=dtype
+            (self.points, 1, 1,), dtype=dtype
         )  # fading memory control
 
-        self.M = cp.zeros(
-            (self.num_points, dim_z, dim_z,), dtype=dtype
-        )  # process-measurement cross correlation
-
-        self.z = cp.empty((self.num_points, dim_z, 1,), dtype=dtype)
+        self.z = cp.empty((self.points, dim_z, 1,), dtype=dtype)
 
         # Allocate GPU resources
         threads_z_axis = 16
@@ -138,33 +287,27 @@ class KalmanFilter(object):
             _filters.GPUKernel.UPDATE,
         )
 
-        # print("Predict")
-        # print(self.predict_kernel.kernel.const_size_bytes)
-        # print(self.predict_kernel.kernel.local_size_bytes)
-        # print(self.predict_kernel.kernel.max_dynamic_shared_size_bytes)
-        # print(self.predict_kernel.kernel.max_threads_per_block)
-        # print(self.predict_kernel.kernel.num_regs)
-        # print(self.predict_kernel.kernel.shared_size_bytes)
-        # print()
-        # print("Update")
-        # print(self.update_kernel.kernel.const_size_bytes)
-        # print(self.update_kernel.kernel.local_size_bytes)
-        # print(self.update_kernel.kernel.max_dynamic_shared_size_bytes)
-        # print(self.update_kernel.kernel.max_threads_per_block)
-        # print(self.update_kernel.kernel.num_regs)
-        # print(self.update_kernel.kernel.shared_size_bytes)
-        # print()
-        # print(max_threads_per_block)
-        # print(min_blocks_per_multiprocessor)
-        # print()
-
     def predict(self):
+        """
+        Predict next state (prior) using the Kalman filter state propagation
+        equations.
+
+        Parameters
+        ----------
+
+        """
 
         self.predict_kernel(
             self._alpha_sq, self.x, self.F, self.P, self.Q,
         )
 
     def update(self):
+        """
+        Update Kalman Filter.
+
+        Parameters
+        ----------
+        """
 
         self.update_kernel(
             self.x, self.z, self.H, self.P, self.R,
