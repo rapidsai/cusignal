ef81981cf42a9003e84cf3541b07c79f5cd7430b
Mark Sadang <marksadang@engineer.com>
2020-11-13 10:24:23 -0800

    removed gpuci_logger and replaced parallel level

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -5,14 +5,9 @@
 ##########################################
 set -e
 
-# Logger function for build status output
-function gpuci_logger() {
-  echo -e "\n>>>> $@\n"
-}
-
 # Set path and build parallel level
 export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
-export PARALLEL_LEVEL=-4
+export PARALLEL_LEVEL=${PARALLEL_LEVEL:-4}
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
