36493cd65d31b7459809a6bcd2271320111a9815
AJ Schmidt <aschmidt@nvidia.com>
2021-05-19 11:43:01 -0400

    remove NIGHTLY_VERSION var

--- ci/docs/build.sh
+++ ci/docs/build.sh
@@ -15,7 +15,6 @@ export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
 export HOME=$WORKSPACE
 export PROJECT_WORKSPACE=/rapids/cusignal
 export LIBCUDF_KERNEL_CACHE_PATH="$HOME/.jitify-cache"
-export NIGHTLY_VERSION=$(echo $BRANCH_VERSION | awk -F. '{print $2}')
 export PROJECTS=(cusignal)
 
 gpuci_logger "Check environment"
