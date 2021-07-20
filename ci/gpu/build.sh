a47ba86810f63ace189bba5f4d522b04f83e6568
Adam Thompson <34922235+awthomp@users.noreply.github.com>
2021-07-20 15:50:14 -0400

    Remove pytorch from cusignal CI/CD (#404)
    Fix errors when running CI/CD on cuSignal PRs with PyTorch and CTK 11.2 and 11.0.
    
    cc @ajschmidt8 @jakirkham
    
    Authors:
      - Adam Thompson (https://github.com/awthomp)
    
    Approvers:
      - AJ Schmidt (https://github.com/ajschmidt8)
      - https://github.com/jakirkham
    
    URL: https://github.com/rapidsai/cusignal/pull/404

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -85,9 +85,6 @@ cd $WORKSPACE/python
 
 pytest --cache-clear --junitxml=${WORKSPACE}/junit-cusignal.xml -v -s -m "not cpu"
 
-conda remove -y --force blas nomkl rapids-build-env rapids-notebook-env
-gpuci_conda_retry install -y -c pytorch "pytorch>=1.4"
-
 ${WORKSPACE}/ci/gpu/test-notebooks.sh 2>&1 | tee nbtest.log
 python ${WORKSPACE}/ci/utils/nbtestlog2junitxml.py nbtest.log
 
