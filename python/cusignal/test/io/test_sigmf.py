rapidsai/cusignal

commit de9be48c5cd8b8dc15416cbaf540164c40a9bc94

Fix bug in bin_reader that ignored dtype (#333)
cuSignal's binary reader would ignore specified dtype, causing an error in parsing a stored binary file. The following code example now produces output `y` that matches input `x`.

```python
import cupy as cp
import cusignal
x = cp.arange(1000,dtype=cp.float32)
cusignal.write_bin('bintest',x)
y = cusignal.read_bin('bintest',dtype=cp.float32)
```

Authors:
  - Adam Thompson (@awthomp)

Approvers:
  - Brad Rees (@BradReesWork)

URL: https://github.com/rapidsai/cusignal/pull/333

python/cusignal/io/reader.py
@@ -76,7 +76,8 @@ def read_bin(file, buffer=None, dtype=cp.uint8, num_samples=None, offset=0):
     # offset is measured in bytes
     offset *= cp.dtype(dtype).itemsize
 
-    fp = np.memmap(file, mode="r", offset=offset, shape=num_samples)
+    fp = np.memmap(file, mode="r", offset=offset, shape=num_samples,
+                   dtype=dtype)
 
     if buffer is not None:
         out = cp.empty(buffer.shape, buffer.dtype)

commit d69f9cbdafb028bc80eb5fd1d9c547e82af0f91f

Fix pinned buffer IO issues (#479)
This PR resolve pinned buffer IO issues introduced in https://github.com/rapidsai/cusignal/commit/de9be48c5cd8b8dc15416cbaf540164c40a9bc94; essentially, if `read_bin` is passed a `buffer` and `dtype`, it should prioritize `buffer.dtype` over `dtype` to ensure that the buffer and memmap sizes match up.

Still want to add IO tests to ensure that things are working properly, and potentially alter `read_bin` to raise an error if both `buffer` and `dtype` are provided.

Closes #474

Authors:
  - Charles Blackmon-Luca (https://github.com/charlesbluca)

Approvers:
  - Adam Thompson (https://github.com/awthomp)

URL: https://github.com/rapidsai/cusignal/pull/479

python/cusignal/io/reader.py
@@ -73,6 +73,10 @@ def read_bin(file, buffer=None, dtype=cp.uint8, num_samples=None, offset=0):
     # Get current stream, default or not.
     stream = cp.cuda.get_current_stream()
 
+    # prioritize dtype of buffer if provided
+    if buffer is not None:
+        dtype = buffer.dtype
+
     # offset is measured in bytes
     offset *= cp.dtype(dtype).itemsize
 
@@ -80,13 +84,11 @@ def read_bin(file, buffer=None, dtype=cp.uint8, num_samples=None, offset=0):
                    dtype=dtype)
 
     if buffer is not None:
-        out = cp.empty(buffer.shape, buffer.dtype)
-
-    if buffer is None:
-        out = cp.asarray(fp)
-    else:
         buffer[:] = fp[:]
+        out = cp.empty(buffer.shape, buffer.dtype)
         out.set(buffer)
+    else:
+        out = cp.asarray(fp)
 
     stream.synchronize()
 

python/cusignal/test/io/test_bin.py
@@ -1,0 +1,101 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cupy as cp
+
+import cusignal
+
+
+def test_read_paged(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    actual.tofile(data_fname)
+
+    expect = cusignal.read_bin(str(data_fname), dtype=cp.complex64)
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_read_pinned_buffer(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    actual.tofile(data_fname)
+
+    binary = cusignal.read_bin(str(data_fname), dtype=cp.complex64)
+    buffer = cusignal.get_pinned_mem(binary.shape, cp.complex64)
+
+    expect = cusignal.read_bin(str(data_fname), buffer, dtype=cp.complex64)
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_read_shared_buffer(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    actual.tofile(data_fname)
+
+    binary = cusignal.read_bin(str(data_fname), dtype=cp.complex64)
+    buffer = cusignal.get_shared_mem(binary.shape, cp.complex64)
+
+    expect = cusignal.read_bin(str(data_fname), buffer, dtype=cp.complex64)
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_write_paged(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+
+    cusignal.write_bin(str(data_fname), actual)
+
+    expect = cusignal.read_bin(str(data_fname), dtype=cp.complex64)
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_write_pinned_buffer(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+
+    cusignal.write_bin(str(data_fname), actual)
+
+    binary = cusignal.read_bin(str(data_fname), dtype=cp.complex64)
+    buffer = cusignal.get_pinned_mem(binary.shape, cp.complex64)
+
+    cusignal.write_bin(str(data_fname), actual, buffer=buffer, append=False)
+
+    expect = cusignal.read_bin(str(data_fname), buffer=buffer)
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_write_shared_buffer(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+
+    cusignal.write_bin(str(data_fname), actual)
+
+    binary = cusignal.read_bin(str(data_fname), dtype=cp.complex64)
+    buffer = cusignal.get_shared_mem(binary.shape, cp.complex64)
+
+    cusignal.write_bin(str(data_fname), actual, buffer=buffer, append=False)
+
+    expect = cusignal.read_bin(str(data_fname), buffer=buffer)
+
+    cp.testing.assert_array_equal(actual, expect)

python/cusignal/test/io/test_sigmf.py
@@ -1,0 +1,123 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import cupy as cp
+
+import cusignal
+
+
+def test_read_paged(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+    meta_fname = tmpdir.join("test_read.sigmf-meta")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    meta = {"core:datatype": "cf32"}
+
+    actual.tofile(data_fname)
+    meta_fname.write(json.dumps(meta))
+
+    expect = cusignal.read_sigmf(str(data_fname), str(meta_fname))
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_read_pinned_buffer(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+    meta_fname = tmpdir.join("test_read.sigmf-meta")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    meta = {"core:datatype": "cf32"}
+
+    actual.tofile(data_fname)
+    meta_fname.write(json.dumps(meta))
+
+    binary = cusignal.read_bin(str(data_fname))
+    buffer = cusignal.get_pinned_mem(binary.shape, cp.ubyte)
+
+    expect = cusignal.read_sigmf(str(data_fname), str(meta_fname), buffer)
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_read_shared_buffer(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+    meta_fname = tmpdir.join("test_read.sigmf-meta")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    meta = {"core:datatype": "cf32"}
+
+    actual.tofile(data_fname)
+    meta_fname.write(json.dumps(meta))
+
+    binary = cusignal.read_bin(str(data_fname))
+    buffer = cusignal.get_shared_mem(binary.shape, cp.ubyte)
+
+    expect = cusignal.read_sigmf(str(data_fname), str(meta_fname), buffer)
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_write_paged(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+    meta_fname = tmpdir.join("test_read.sigmf-meta")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    meta = {"core:datatype": "cf32"}
+
+    cusignal.write_sigmf(str(data_fname), actual)
+    meta_fname.write(json.dumps(meta))
+
+    expect = cusignal.read_sigmf(str(data_fname), str(meta_fname))
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_write_pinned_buffer(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+    meta_fname = tmpdir.join("test_read.sigmf-meta")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    meta = {"core:datatype": "cf32"}
+
+    cusignal.write_bin(str(data_fname), actual)
+    meta_fname.write(json.dumps(meta))
+
+    binary = cusignal.read_bin(str(data_fname))
+    buffer = cusignal.get_pinned_mem(binary.shape, cp.ubyte)
+
+    cusignal.write_sigmf(str(data_fname), actual, buffer=buffer, append=False)
+
+    expect = cusignal.read_sigmf(str(data_fname), str(meta_fname))
+
+    cp.testing.assert_array_equal(actual, expect)
+
+
+def test_write_shared_buffer(tmpdir):
+    data_fname = tmpdir.join("test_read.sigmf-data")
+    meta_fname = tmpdir.join("test_read.sigmf-meta")
+
+    actual = cp.random.rand(100).astype(cp.complex64)
+    meta = {"core:datatype": "cf32"}
+
+    cusignal.write_bin(str(data_fname), actual)
+    meta_fname.write(json.dumps(meta))
+
+    binary = cusignal.read_bin(str(data_fname))
+    buffer = cusignal.get_shared_mem(binary.shape, cp.ubyte)
+
+    cusignal.write_sigmf(str(data_fname), actual, buffer=buffer, append=False)
+
+    expect = cusignal.read_sigmf(str(data_fname), str(meta_fname))
+
+    cp.testing.assert_array_equal(actual, expect)