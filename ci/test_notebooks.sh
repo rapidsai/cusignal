dbbfd349538ef958a23fcd3ea7b047c21305ab27
AJ Schmidt <aschmidt@nvidia.com>
2022-12-27 15:41:52 -0500

    skip `rtlsdr_offline_demod_to_transcript.ipynb` notebook testing
    it looks like this notebook test requires a connection to an external device.
    
    the job fails in CI since this device isn't available

--- ci/test_notebooks.sh
+++ ci/test_notebooks.sh
@@ -32,7 +32,7 @@ pushd notebooks
 
 # Add notebooks that should be skipped here
 # (space-separated list of filenames without paths)
-SKIPNBS="sdr_wfm_demod.ipynb io_examples.ipynb"
+SKIPNBS="sdr_wfm_demod.ipynb io_examples.ipynb rtlsdr_offline_demod_to_transcript.ipynb"
 
 # Set SUITEERROR to failure if any run fails
 SUITEERROR=0
